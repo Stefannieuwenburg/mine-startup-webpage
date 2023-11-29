758216a297e3020ae014a1de28b12fc796853987
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2023-11-29 01:14:56 +0100

    commit

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -3,34 +3,26 @@ import Image from "../../assets/img/about.jpg";
 const About = () => {
   return (
     <section id="about">
-      <div className="min-h-screen w-full bg-blue-100 text-white-100 mt-12 text-center text-4xl ">
-        <div className="flex flex-row text-center ">
-          <h2 className="text-white  text-xl mt-20 mx-4">
-            <span className="text-5xl m-4">About</span> <br /> Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s, when an unknown printer
-            took a galley of type and scrambled Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled Lorem Ipsum has been the
-            industry's standard
-          </h2>
-          <div>
-            <div className="w-1/3 mt-20">
-              <img className="max-w-full img-responsive " src={Image} alt="" />
+     <div class="flex pt-12 px-6 md:px-20  items-center justify-center bg-hero md:h-screen overflow-hidden">
+    <div class="flex flex-col  gap-6 md:flex-row items-center max-w-8xl">
+        <div class="w-full md:w-1/2 lg:pr-32">
+            <h2 class="text-4xl lg:text-5xl text-center md:text-left text-blue-900 leading-tight font-medium">There’s a
+                better way to talk with your customers.</h2>
+            <h3
+                class="mt-6 md:mt-10 text-md lg:text-xl text-center md:text-left text-gray-700 font-light tracking-wider leading-relaxed">
+                Help Scout is designed with your customers in mind. Provide email and live chat with a personal touch,
+                and deliver help content right where your customers need it, all in one place, all for one low price.
+            </h3>
+            <div class="mt-10 flex flex-col sm:flex-row justify-center md:justify-start">
+                <button class="w-full sm:w-40 px-4 py-3 rounded font-semibold text-md bg-blue-500 text-white border-2 border-blue-500">Info</button>
+                <button class="w-full mt-4 sm:mt-0 sm:ml-4 sm:w-40 px-4 py-3 rounded font-semibold text-md bg-white text-blue-500 border-2 border-gray-500">Watch a Demo</button>
             </div>
-            <h4 className="mt-20 mx-4 text-center text-2xl w-2/3">
-              Lorem Ipsum has been the industry's standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled Lorem Ipsum has been the industry's standard
-              dummy text ever since the 1500s, when an unknown printer took a
-              galley of type and scrambled Lorem Ipsum has been the industry's
-              standard dummy text ever since the 1500s, when an unknown printer
-              took a galley of type and scrambled
-            </h4>
-          </div>
         </div>
-      </div>
+        <div class="w-full md:w-1/2 flex justify-center md:justify-end">
+            <img src={Image} alt="hero"/>
+        </div>
+    </div>
+</div>
     </section>
   );
 };
