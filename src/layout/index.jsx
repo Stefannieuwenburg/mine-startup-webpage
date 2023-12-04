7c8dab03a5f7e0ed4117d4cf95b8ff797c9c7f2e
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2023-12-04 23:24:07 +0100

    commit

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,5 +1,5 @@
-import Navbar from "../navbar";
-import Footer from "../footer";
+import Navbar from "../componets/navbar";
+import Footer from "../componets/footer";
 
 const Layout = ({ children }) => {
   return (
