f9c04d35770c0576d09d5a43760c2aa182de59ee
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2024-01-08 17:03:07 +0100

    commit

--- src/componets/navbar/index.jsx
+++ src/componets/navbar/index.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import {Link} from "react-scroll";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import logo from "../../assets/logos/logo.png";
 import { FaWhatsapp } from "react-icons/fa";
 
 const navigation = [
@@ -24,12 +23,6 @@ const Navbar = () => {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <img className="h-16 w-16" src={logo} alt="" />
-          </a>
-          <span className="text-center m-4 text-2xl font-semibold">
-            Prof. Dr. Zahide Mine Yazıcı
-          </span>
         </div>
         <div className="text-3xl font-semibold leading-6"></div>
         <div className="flex lg:hidden">
@@ -78,10 +71,7 @@ const Navbar = () => {
         <div className="fixed inset-0 z-50" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <img className="h-20 w-auto" src={logo} alt="" />
-              <span className="">Prof. Dr. Zahide Mine Yazıcı</span>
-            </a>
+              <span className="text-xl">Prof. Dr. Zahide Mine Yazıcı</span>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
